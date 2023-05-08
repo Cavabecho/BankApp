8ce608ab425a9aa8d68716f366fb42e9d7e896df
Cavabecho <115113171+Cavabecho@users.noreply.github.com>
2023-05-08 20:44:19 +0200

    Looks like i made a started greetings

--- src/danylo/bank/app/Main.java
+++ src/danylo/bank/app/Main.java
@@ -1,71 +1,11 @@
 package danylo.bank.app;
 
-import danylo.bank.app.bankers.JuniorBanker;
-import danylo.bank.app.bankers.MiddleBanker;
-import danylo.bank.app.bankers.SeniorBanker;
-import danylo.bank.app.customers.BronzeCustomer;
-import danylo.bank.app.customers.GoldCustomer;
-import danylo.bank.app.customers.SilverCustomer;
 
 /**
  * This file starts execution of the program.
  */
 public class Main {
     public static void main(String[] args) {
-//        BronzeCustomer bz = new BronzeCustomer("Nikita", 1, 10000.0);
-//        BronzeCustomer bz2 = new BronzeCustomer("Danilo", 2, 100000.0);
-//        JuniorBanker jb = new JuniorBanker("Danilo", "junior", 1);
-//        DaniloBank daniloBank = new DaniloBank();
-//
-//        System.out.println(bz.getBalance());
-//        System.out.println(bz2.getBalance());
-//        System.out.println(DaniloBank.getTotalBankAccount());
-//
-//        bz.topUpBalance(100.0, jb);
-//        bz2.topUpBalance(100.0,jb);
-//
-//        System.out.println(bz.getBalance());
-//        System.out.println(bz2.getBalance());
-//        System.out.println(DaniloBank.getTotalBankAccount());
-//
-//        bz.withdrawMoney(233.0, jb);
-//        bz2.withdrawMoney(56.0, jb);
-//
-//        System.out.println(bz.getBalance());
-//        System.out.println(bz2.getBalance());
-//        System.out.println(DaniloBank.getTotalBankAccount());
-
-
-//        SilverCustomer sc = new SilverCustomer("Maksim", 3, 10000.0);
-//        MiddleBanker mb = new MiddleBanker("Danilo", "middle", 2);
-//        DaniloBank daniloBank = new DaniloBank();
-//
-//        System.out.println(sc.getBalance());
-//        System.out.println(DaniloBank.getTotalCustomersCredit());
-//
-//        sc.takeCredit(5007.9, mb);
-//
-//        System.out.println(sc.getBalance());
-//        System.out.println(DaniloBank.getTotalCustomersCredit());
-
-
-//        GoldCustomer gd = new GoldCustomer("Dsd", 3, 50000.0);
-//        SeniorBanker sb = new SeniorBanker("Danilo", "middle", 2);
-//        DaniloBank daniloBank = new DaniloBank();
-//
-//        System.out.println(DaniloBank.getTotalBankAccount());
-//        System.out.println(gd.getBalance());
-//
-//        gd.topUpBalance(6000.0, sb);
-//
-//
-//        System.out.println(DaniloBank.getTotalBankAccount());
-//        System.out.println(gd.getBalance());
-//
-//        gd.takeCredit(5000.0, sb);
-//
-//        System.out.println(DaniloBank.getTotalBankAccount());
-//        System.out.println(DaniloBank.getTotalCustomersCredit());
-//        System.out.println(gd.getBalance());
+        ConsoleUI.displayGreetings();
     }
 }
